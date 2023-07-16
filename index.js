da470fd4f4fbb0a3dc0d5f3dbf3d78963ab255f2
Ali <ahaba066@uottawa.ca>
2023-07-16 15:05:40 -0400

    home page and click function

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1. Detect clicks on the page
+// 2. For each click, increment the counter variable by 1
+// 3. Update the counter paragraph with the new value
+
+let count = 0;
+
+addEventListener('click', () => {
+    count += 1;
+    document.getElementById('counter').innerHTML = count;
+})
